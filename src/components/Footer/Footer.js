ebd6859fb14627fe0f341086a2f33d6deacfeb17
Nevilkumar <nevil.0619@gmail.com>
2022-01-07 11:58:02 +0530

    updated

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ import './Footer.css';
 const Footer = () => {
     return (
         <div className='footer-container'>
-            Made by Nevil Dalsania | React Project for WoC 4.0 - 2021 | Copyrights &copy; Reserved
+            Made by Nevil Dalsania | React Project for WoC 4.0 - 2022 | Copyrights &copy; Reserved
         </div>
     )
 }

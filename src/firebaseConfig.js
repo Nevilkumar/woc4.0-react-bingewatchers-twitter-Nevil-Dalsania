a93c1bef98a3bcfbea58216991f8bbe50e35da07
Nevilkumar <nevil.0619@gmail.com>
2022-01-20 17:26:41 +0530

    FirebaseConfig Added

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -4,12 +4,12 @@ import 'firebase/compat/auth';
 import 'firebase/compat/storage';
 
 const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
+  apiKey: "AIzaSyBWLw4P9wqtcejvwQwiFTXkPxZrWXsXeNU",
+  authDomain: "twitter-85307.firebaseapp.com",
+  projectId: "twitter-85307",
+  storageBucket: "twitter-85307.appspot.com",
+  messagingSenderId: "709386510644",
+  appId: "1:709386510644:web:c8f1d5c7398d1f1838c561"
 });
 
 
